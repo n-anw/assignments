9f6a7af23ad5e26fed4e02dfcbf1ed82ef9d3d6a
n-anw <nanway29@gmail.com>
2020-01-10 11:34:37 -0500

    remove old files

--- add1.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import sys
-input_number = sys.stdin.read()
-print(int(input_number) + 1)
--- aplusb.py
+++
@@ -1,5 +0,0 @@
-#This program takes inputs a and b and ads them, printing the result in a string
-
-a = 5
-b = 7
-print("The sum of a and b is " + str(a + b))
