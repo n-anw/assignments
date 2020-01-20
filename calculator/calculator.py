0eeaef412e6c881e1a9c186a6e35e9a88f965f2d
n-anw <nanway29@gmail.com>
2020-01-20 13:46:24 -0500

    add functions to calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,38 @@
+# Nick funcitons HW
+
+# define a and b variable for testing
+
+a = 2
+b = 3
+
+# define a funtion multiply
+
+def multiply(a, b):
+	return a * b
+
+# test function
+# print(multiply(a, b))
+
+# define a function add
+
+def add(a, b):
+	return a + b
+
+# test function
+# print(add(a, b))
+
+# define a fuction subtract
+
+def subtract(a, b):
+	return a - b
+
+# test function
+# print(subtract(a, b))
+
+# define a function divide
+
+def divide(a, b):
+	return a / b
+
+# test function
+# print (divide(a, b))
