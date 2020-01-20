cf38e5393393b14cc10eaa80ec368cecb2479514
n-anw <nanway29@gmail.com>
2020-01-20 14:04:30 -0500

    add functions to calculator in js

---
+++ calculator/calculator.js
@@ -0,0 +1,36 @@
+// Nick funcitons HW
+
+// define a and b variable for testing
+
+var a = 2
+var b = 3
+
+// define a function multiply
+
+function multiply(a,b) { 
+    return a * b
+}
+
+// define a function add
+
+function add(a,b) {
+	return a + b
+}
+
+// define a function subtract
+
+function subtract(a,b) {
+	return a - b
+}
+
+// define a function divide
+
+function divide(a,b) {
+	return a / b
+}
+
+// step 3
+
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
