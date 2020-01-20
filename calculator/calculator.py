c695271ec8441dca9da168cbd1d5a588d5b3efed
n-anw <nanway29@gmail.com>
2020-01-20 13:57:48 -0500

    add bonus

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -41,3 +41,31 @@ def divide(a, b):
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+# bonus
+
+def square(a):
+	return a ** 2
+
+def cube(a):
+	return a ** 3
+
+
+
+def square_n_times(number, n):
+	if n > 0:
+		output = 0
+		while n > 0:
+			output += number ** 2
+			n -= 1
+		return output
+	else:
+		print("Use a positive int for n!")
+
+# test function
+# number = 2
+# n = 2
+# print(square_n_times(number, n))
+
+
+
